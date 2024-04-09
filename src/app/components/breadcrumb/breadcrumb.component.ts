5c99ddb293d8ccf3bf6fc131410773a377abbf76
Diego Manuel Jaeger Rapoport <diegomrapoport@gmail.com>
2024-04-08 21:23:33 -0300

    add breadcrumb component

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss'
 })
-export class BreadcrumbComponent {
+export class BreadcrumbComponent implements OnInit {
+
+  breadcrumbPath: string[] = []
+  currentUrl: string = ''
+
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.router.events.subscribe((ev) => {
+      if(ev instanceof NavigationEnd) {
+        this.currentUrl = ev.urlAfterRedirects
+        this.breadcrumbPath = ev.urlAfterRedirects.split('/').filter((url: string) => url)
+      }
+    })
+  }
 
 }
